5427be4692c2b61d6d17353836924e2e72b92cc8
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-02-19 15:31:58 +0100

    first commit

---
+++ Backend/src/main/java/com/HolosINC/Holos/HolosApplication.java
@@ -0,0 +1,13 @@
+package com.HolosINC.Holos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolosApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/HolosINC/Holos/HolosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HolosINC.Holos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
