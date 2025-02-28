99700340428cdfbfeeb8060cde030ee0a24b2fc4
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-02-28 16:29:37 +0100

    feat: Data population and petclinic adaptation

---
+++ Backend/src/main/java/com/HolosINC/Holos/client/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.HolosINC.Holos.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+}
