7ddbc5d1d5980496bd6fce43a2bcc2733e7f54ec
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-02-28 22:27:47 +0100

    fix: data population

---
+++ Backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO base_user (id, first_name, username, password, email, phone_number, image_profile, created_user) VALUES
+(1, 'John', 'john_doe', 'password123', 'john.doe@example.com', '1234567890', 'profile1.jpg', '2025-02-28'),
+(2, 'Jane', 'jane_doe', 'password456', 'jane.doe@example.com', '0987654321', 'profile2.jpg', '2025-02-28'),
+(3, 'Yellow', 'yellow', 'password', 'yellow.artist@gmail.com', '0987654321', 'profile3.jpg', '2025-02-28');
+
+INSERT INTO clients (id, base_user_id) VALUES
+(1, 1),
+(2, 2);
+
+INSERT INTO artists (id, base_user_id, num_slots_of_work, table_commisions_price) VALUES
+(1, 3, 3,'tabla.jpg');
--- Backend/src/main/resources/static/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO base_user (id, first_name, username, password, email, phone_number, image_profile, created_user) VALUES
-(1, 'John', 'john_doe', 'password123', 'john.doe@example.com', '1234567890', 'profile1.jpg', '2025-02-28'),
-(2, 'Jane', 'jane_doe', 'password456', 'jane.doe@example.com', '0987654321', 'profile2.jpg', '2025-02-28');
-
-INSERT INTO client (id, base_user_id) VALUES
-(1, 1),
-(2, 2);
