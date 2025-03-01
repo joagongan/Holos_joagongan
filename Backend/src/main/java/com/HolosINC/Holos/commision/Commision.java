2bfacc21e41405a8d54cd9084f622a6e4a079fef
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-03-01 20:20:31 +0100

    feat: Commision entity created

---
+++ Backend/src/main/java/com/HolosINC/Holos/commision/Commision.java
@@ -0,0 +1,44 @@
+package com.HolosINC.Holos.commision;
+
+import java.util.Date;
+
+import com.HolosINC.Holos.work.Work;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "commisions")
+@Data
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Commision extends Work{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private StatusCommision status;
+
+    private Integer numMilestones;
+
+    @Temporal(TemporalType.DATE)
+    private Date acceptedDateByArtist;
+
+    @Enumerated(EnumType.STRING)
+    private EnumPaymentArrangement paymentArrangement;
+
+    // @OneToMany(mappedBy = "commision", cascade = CascadeType.ALL, orphanRemoval = true)
+    // private List<Milestones> milestones;
+}
---
+++ Backend/src/main/java/com/HolosINC/Holos/work/Work.java
@@ -0,0 +1,5 @@
+package com.HolosINC.Holos.work;
+
+public class Work {
+    
+}
