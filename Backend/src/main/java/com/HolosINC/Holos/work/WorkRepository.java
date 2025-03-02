847a0c603edb2aabadac1baaa704143e98255b34
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-03-02 11:25:06 +0100

    feat: WorkRepository created

---
+++ Backend/src/main/java/com/HolosINC/Holos/work/WorkRepository.java
@@ -0,0 +1,9 @@
+package com.HolosINC.Holos.work;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkRepository extends JpaRepository<Work, Long>{
+    
+}
