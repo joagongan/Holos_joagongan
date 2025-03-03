31377bca8311fc463277cb3f864fae23b7a596f4
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-03-03 12:18:24 +0100

    fix: Id GenerationType changed

--- Backend/src/main/java/com/HolosINC/Holos/work/Work.java
+++ Backend/src/main/java/com/HolosINC/Holos/work/Work.java
@@ -24,7 +24,7 @@ import lombok.Data;
 public class Work {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
