ca8ce948f5caf00f625081b4b3e3d2dd1fa32f2e
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-03-03 12:09:26 +0100

    fix: InheritanceType and relationship with Category entity changed

--- Backend/src/main/java/com/HolosINC/Holos/work/Work.java
+++ Backend/src/main/java/com/HolosINC/Holos/work/Work.java
@@ -1,5 +1,7 @@
 package com.HolosINC.Holos.work;
 
+import java.util.Set;
+
 import com.HolosINC.Holos.artist.Artist;
 
 import jakarta.persistence.Entity;
@@ -9,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -16,9 +20,9 @@ import lombok.Data;
 @Entity
 @Table(name = "works")
 @Data
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Work {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,10 +31,10 @@ public class Work {
     private String description;
     private Double price;
 
-    // @ManyToOne
-    // @JoinColumn(name = "category_id")
-    // private Category category;
-    
+    // @ManyToMany
+    // @JoinTable(name = "work_category", joinColumns = @JoinColumn(name = "work_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    // private Set<Category> categories;
+
     @ManyToOne
     @JoinColumn(name = "artist_id", nullable = false)
     private Artist artist;
