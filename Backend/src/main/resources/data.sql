e71fed9cbec6d7ba8e829fe6168eb8df8088a206
GabriVG <gabrivacarog@gmail.com>
2025-03-06 23:40:12 +0100

    comentado lo que no funcioan

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,25 +1,27 @@
+/*
 INSERT INTO clients (id, first_name, username, password, email, phone_number, image_profile, created_user) VALUES
-(1, 'Jeenii', 'jeenni', 'password001', 'reinapata20@gmail.com', '1234567890', NULL, '2025-03-06'),
-(2, 'Emilio', 'emilio', 'password002', 'emilio.esp99@gmail.com', '1234567890', NULL, '2025-03-06'),
-(3, 'Rafael', 'rafael', 'password003', 'dekker.rafa@gmail.com', '1234567890', NULL, '2025-03-06'),
-(4, 'Felix', 'felix', 'password004', 'felixsv26@gmail.com', '1234567890', NULL, '2025-03-06'),
-(5, 'Raúl', 'raul', 'password005', 'raullebone@gmail.com', '1234567890', NULL, '2025-03-06'),
-(6, 'Joker', 'joker', 'password006', 'jokerin.escuchame@gmail.com', '1234567890', NULL, '2025-03-06'),
-(7, 'Lucas', 'lucas', 'password007', 'lmherencia2003@gmail.com', '1234567890', NULL, '2025-03-06'),
-(8, 'Horacio', 'horacio', 'password008', 'geko.hor@gmail.com', '1234567890', NULL, '2025-03-06'),
-(9, 'Simón', 'simon', 'password009', 'simoncgs03@gmail.com', '1234567890', NULL, '2025-03-06'),
-(10, 'Javier', 'javies_pacheco', 'password010', 'pachecomarquezjavier@gmail.com', '1234567890', NULL, '2025-03-06'),
-(11, 'Henrique', 'henrique', 'password011', 'henrique.rebolho@gmail.com', '1234567890', NULL, '2025-03-06'),
-(12, 'Keegan', 'kaegan', 'password012', 'Kcullen3289@gmail.com', '1234567890', NULL, '2025-03-06'),
-(13, 'Carlos', 'carlos', 'password013', 'carlosperezsanchez199@gmail.com', '1234567890', NULL, '2025-03-06'),
-(14, 'Milagros', 'milagros', 'password014', 'milagros.huerta@gm.uca.es', '1234567890', NULL, '2025-03-06'),
-(15, 'Patricia', 'patricia', 'password015', 'kaleta1979@gmail.com', '1234567890', NULL, '2025-03-06'),
+(1, 'Jeenii', 'jeenni', 'password001', 'reinapata20@gmail.com', '123456789', NULL, '2025-03-06'),
+(2, 'Emilio', 'emilio', 'password002', 'emilio.esp99@gmail.com', '123456789', NULL, '2025-03-06'),
+(3, 'Rafael', 'rafael', 'password003', 'dekker.rafa@gmail.com', '123456789', NULL, '2025-03-06'),
+(4, 'Felix', 'felix', 'password004', 'felixsv26@gmail.com', '123456789', NULL, '2025-03-06'),
+(5, 'Raúl', 'raul', 'password005', 'raullebone@gmail.com', '123456789', NULL, '2025-03-06'),
+(6, 'Joker', 'joker', 'password006', 'jokerin.escuchame@gmail.com', '123456789', NULL, '2025-03-06'),
+(7, 'Lucas', 'lucas', 'password007', 'lmherencia2003@gmail.com', '123456789', NULL, '2025-03-06'),
+(8, 'Horacio', 'horacio', 'password008', 'geko.hor@gmail.com', '123456789', NULL, '2025-03-06'),
+(9, 'Simón', 'simon', 'password009', 'simoncgs03@gmail.com', '123456789', NULL, '2025-03-06'),
+(10, 'Javier', 'javies_pacheco', 'password010', 'pachecomarquezjavier@gmail.com', '123456789', NULL, '2025-03-06'),
+(11, 'Henrique', 'henrique', 'password011', 'henrique.rebolho@gmail.com', '123456789', NULL, '2025-03-06'),
+(12, 'Keegan', 'kaegan', 'password012', 'Kcullen3289@gmail.com', '123456789', NULL, '2025-03-06'),
+(13, 'Carlos', 'carlos', 'password013', 'carlosperezsanchez199@gmail.com', '123456789', NULL, '2025-03-06'),
+(14, 'Milagros', 'milagros', 'password014', 'milagros.huerta@gm.uca.es', '123456789', NULL, '2025-03-06'),
+(15, 'Patricia', 'patricia', 'password015', 'kaleta1979@gmail.com', '123456789', NULL, '2025-03-06'),
 (16,'Daniel', 'daniel', 'password016', 'megamagolas@gmail.com', '663522963', NULL, '2025-03-06'),
 (17,'Rafael', 'rafaelduque', 'password017', 'rafduqcol@alum.us.es', '722675760', NULL, '2025-03-06'),
 (18,'Rafael', 'rafaelcastillo', 'password018', 'rafaelcastillocebolla@gmail.com', '671168164', NULL, '2025-03-06'),
 (19,'Mohamed', 'mohamed', 'password019', 'mohmmedabourihhh@gmail.com', '602171961', NULL, '2025-03-06'),
 (20,'Enrique', 'enrique', 'password020', 'kiquegaraba@gmail.com', '600619217', NULL, '2025-03-06');
 
+*/
 INSERT INTO artists (id, first_name, username, password, email, phone_number, image_profile, created_user, num_slots_of_work, table_commisions_price) VALUES
 (1,'Braulio', 'braulio', 'password021', 'braulioolmedo116@gmail.com', '1234567890', NULL, '2025-03-06', 0, NULL),
 (2,'Claudia', 'claudia', 'password022', 'xlaequis@gmail.com', '1234567890', NULL, '2025-03-06', 0, NULL),
@@ -58,6 +60,7 @@ INSERT INTO works (id, artist_id, name, description, price) VALUES
 (4, 1, 'Abstract Art', 'A modern abstract composition', 180.0),
 (5, 1, 'Forest Path', 'A peaceful forest pathway', 120.0);
 
+/*
 INSERT INTO milestones (id, name, accepted, milestone_date, commision_id) VALUES 
 (1, 'Initial Sketch', TRUE, '2025-03-02', 1),
 (2, 'Line Art', FALSE, '2025-03-05', 1),
@@ -80,6 +83,7 @@ INSERT INTO milestones (id, name, accepted, milestone_date, commision_id) VALUES
 (19, 'Animation Draft', TRUE, '2025-03-05', 10),
 (20, 'Final Review', FALSE, '2025-03-14', 10);
 
+
 INSERT INTO commisions (id, artist_id, client_id, status, num_milestones, accepted_date_by_artist, payment_arrangement, status_kanban_order_id) VALUES 
 (1, 1, 1, 'REQUESTED', 3, '2025-03-01', 'INITIAL_PAYMENT', 1),
 (2, 2, 2, 'WAITLISTED', 5, '2025-03-02', 'FIFTY_FIFTY', 2),
@@ -91,22 +95,22 @@ INSERT INTO commisions (id, artist_id, client_id, status, num_milestones, accept
 (8, 8, 8, 'ACCEPTED', 4, '2025-03-03', 'FIFTY_FIFTY', 8),
 (9, 9, 9, 'CANCELED', 3, '2025-03-01', 'MODERATED_PAYMENT', 9),
 (10, 10, 10, 'WAITLISTED', 5, '2025-03-06', 'INITIAL_PAYMENT', 1);
-
-INSERT INTO status_kanban_order (id, artist_id, name, "order", description, color) VALUES 
+*/
+/*
+INSERT INTO status_kanban_order (id, artist_id, name, order, description, color) VALUES 
 (1, 1, 'To Do', 1, 'Tasks that need to be started', '#FF5733'),
-(2, 1, 'In Progress', 2, 'Tasks that are currently being worked on', '#33FF57'),
-(3, 1, 'Review', 3, 'Tasks that need to be reviewed before completion', '#3357FF'),
-(4, 1, 'Completed', 4, 'Tasks that have been finished', '#F1C40F'),
-(5, 1, 'Archived', 5, 'Tasks that are no longer active but stored for reference', '#8E44AD'),
-(6, 1, 'Idea', 6, 'New concept or idea being explored', '#FFAA33'),
-(7, 1, 'Sketching', 7, 'Initial sketches and drafts', '#33A8FF'),
-(8, 1, 'Coloring', 8, 'Adding colors to the artwork', '#A833FF'),
-(9, 1, 'Final Touches', 9, 'Adding final adjustments before completion', '#33FFA8'),
-(10, 1, 'Published', 10, 'The artwork is completed and shared', '#FF338A');
-
+(2, 2, 'In Progress', 2, 'Tasks that are currently being worked on', '#33FF57'),
+(3, 3, 'Review', 3, 'Tasks that need to be reviewed before completion', '#3357FF'),
+(4, 4, 'Completed', 4, 'Tasks that have been finished', '#F1C40F'),
+(5, 5, 'Archived', 5, 'Tasks that are no longer active but stored for reference', '#8E44AD'),
+(6, 6, 'Idea', 6, 'New concept or idea being explored', '#FFAA33'),
+(7, 7, 'Sketching', 7, 'Initial sketches and drafts', '#33A8FF'),
+(8, 8, 'Coloring', 8, 'Adding colors to the artwork', '#A833FF'),
+(9, 9, 'Final Touches', 9, 'Adding final adjustments before completion', '#33FFA8'),
+(10, 10, 'Published', 10, 'The artwork is completed and shared', '#FF338A');
+*/
 INSERT INTO works_done (id, artist_id, image) VALUES 
 (1, 1, NULL),
 (2, 1, NULL),
 (3, 1, NULL),
 (4, 1, NULL);
-
