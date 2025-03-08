c44bbb98dc201e855f05d864c1b48d68dfbf2439
jose-maria-portela <92798583+jose-maria-portela@users.noreply.github.com>
2025-03-08 20:22:43 +0100

    fix: data.sql con datos duplicados

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -11,12 +11,6 @@ INSERT INTO categories (id, name, description) VALUES
 (3,'Sculpture', 'Three-dimensional art made by shaping materials');
 
 
-INSERT INTO categories (id, name, description) VALUES
-(1, 'Painting', 'Artworks created using paint on a surface'),
-(2, 'Digital Art', 'Artworks made using digital tools'),
-(3, 'Sculpture', 'Three-dimensional art made by shaping materials');
-
-
 INSERT INTO works (id, artist_id, name, description, price) VALUES 
 (1, 1, 'Sunset Painting', 'A beautiful sunset painting', 150.0),
 (2, 1, 'Ocean Waves', 'A calming ocean scene with waves', 200.0),
