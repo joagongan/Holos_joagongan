b187373711109b25955a29f5b6a5da928c9ff134
juan_antonio_moreno_moguel <juamormog@gmail.com>
2025-03-13 21:15:23 +0100

    Revert "Feature/task 2 tareas de ci cd"

--- Backend/src/main/java/com/HolosINC/Holos/User/User.java
+++ Backend/src/main/java/com/HolosINC/Holos/User/User.java
@@ -1,11 +1,11 @@
 package com.HolosINC.Holos.User;
-import lombok.Data;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
