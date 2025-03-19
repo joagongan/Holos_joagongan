dc6cec840b035c64dcd8ea463e65a7ddab11bc12
JuanAntonioMorenoMoguel <juamormog@gmail.com>
2025-03-19 23:55:07 +0100

    feat: reports controller

---
+++ Backend/src/main/java/com/HolosINC/Holos/reports/ReportService.java
@@ -0,0 +1,68 @@
+package com.HolosINC.Holos.reports;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.HolosINC.Holos.exceptions.ResourceNotFoundException;
+
+@Service
+public class ReportService {
+    
+    private ReportRepository reportRepository;
+
+    private ReportTypeRepository reportTypeRepository;
+
+    @Autowired
+    public ReportService(ReportRepository reportRepository, ReportTypeRepository reportTypeRepository) {
+        this.reportRepository = reportRepository;
+        this.reportTypeRepository = reportTypeRepository;
+    }
+
+    public List<ReportType> getReportTypes() {
+        return reportTypeRepository.findAll();
+    }
+
+    public List<Report> getReports() {
+        return reportRepository.findAll();
+    }
+
+    public Report addReport(Report report) {
+        return reportRepository.save(report);
+    }
+
+    public ReportType addReportType(ReportType reportType) {
+        if (reportTypeRepository.findByType(reportType.getType()).isPresent()) {
+            throw new IllegalArgumentException("Report type name already exists");
+        }
+        return reportTypeRepository.save(reportType);
+    }
+
+    public Report deleteReport(Long id) {
+        Report report = reportRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid report id"));
+        reportRepository.delete(report);
+        return report;
+    }
+    
+    public Report acceptReport(Long id) {
+        Report report = reportRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Report not found"));
+        if (report.getStatus() == ReportStatus.ACCEPTED) {
+            throw new IllegalArgumentException("Report already accepted");
+        }
+        report.setStatus(ReportStatus.ACCEPTED);
+        return reportRepository.save(report);
+    }
+
+    public Report rejectReport(Long id) {
+        Report report = reportRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Report not found"));
+        if (report.getStatus() == ReportStatus.REJECTED) {
+            throw new IllegalArgumentException("Report already rejected");
+        }
+        report.setStatus(ReportStatus.REJECTED);
+        return reportRepository.save(report);
+    }
+}
