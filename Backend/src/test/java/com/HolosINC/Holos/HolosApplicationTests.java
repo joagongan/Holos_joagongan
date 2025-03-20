23316afe0b539289332a8d1f75986322e22fc32c
Joaquín González Ganfornina <joagongan@alum.us.es>
2025-03-20 02:29:49 +0100

    Prueba frontend sin tests

--- Backend/src/test/java/com/HolosINC/Holos/HolosApplicationTests.java
+++ Backend/src/test/java/com/HolosINC/Holos/HolosApplicationTests.java
@@ -1,7 +1,5 @@
 package com.HolosINC.Holos;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,12 +8,7 @@ class HolosApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertTrue(true);
 	}
 
-	@Test
-	void placeholder() {
-        assertTrue(true); // Test vacío para evitar fallos en SonarCloud
-    }
 
 }
