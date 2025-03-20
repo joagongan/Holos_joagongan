5e83e64ade0968342c31c40fac3e76282aa09868
Joaquín González Ganfornina <joagongan@alum.us.es>
2025-03-20 03:09:56 +0100

    Duplicación de código

--- Backend/src/main/java/com/HolosINC/Holos/Kanban/StatusKanbanOrderService.java
+++ Backend/src/main/java/com/HolosINC/Holos/Kanban/StatusKanbanOrderService.java
@@ -11,10 +11,8 @@ import com.HolosINC.Holos.exceptions.ResourceNotFoundException;
 @Service
 public class StatusKanbanOrderService {
 
-
     private StatusKanbanOrderRepository statusKanbanOrderRepository;
     private ArtistService artistService;
-
 
     @Autowired
     public StatusKanbanOrderService(StatusKanbanOrderRepository statusKanbanOrderRepository, ArtistService artistService) {
@@ -27,8 +25,7 @@ public class StatusKanbanOrderService {
         return statusKanbanOrderRepository.save(statusKanbanOrder);
     }
 
-    //Se pone el orden el último. Si no hay nada, el primero por dewfecto
-
+    //Se pone el orden el último. Si no hay nada, el primero por defecto
     @Transactional
     public StatusKanbanOrder addStatusToKanban(String color, String description, String nombre, Integer artistId) {
         StatusKanbanOrder statusKanbanOrder = new StatusKanbanOrder();
@@ -44,7 +41,24 @@ public class StatusKanbanOrderService {
         statusKanbanOrder.setArtist(artistService.findArtist(artistId.longValue()));
         return statusKanbanOrderRepository.save(statusKanbanOrder);
     }
-    
+
+    // Duplicación introducida:
+    @Transactional
+    public StatusKanbanOrder addStatusToKanbanDuplicate(String color, String description, String nombre, Integer artistId) {
+        StatusKanbanOrder statusKanbanOrder = new StatusKanbanOrder();
+        statusKanbanOrder.setColor(color);
+        statusKanbanOrder.setDescription(description);
+        statusKanbanOrder.setName(nombre);
+        List<StatusKanbanOrder> list = statusKanbanOrderRepository.findByArtist(artistId);
+        if(list.isEmpty()){
+            statusKanbanOrder.setOrder(1);  
+        }else{
+            statusKanbanOrder.setOrder(list.size()+1);
+        }
+        statusKanbanOrder.setArtist(artistService.findArtist(artistId.longValue()));
+        return statusKanbanOrderRepository.save(statusKanbanOrder);
+    }
+
     @Transactional
     public StatusKanbanOrder updateStatusKanbanOrder(StatusKanbanOrder statusKanbanOrder) {
         String color="";
@@ -73,68 +87,63 @@ public class StatusKanbanOrderService {
             return statusKanbanOrderRepository.save(sk);
         }else{
             List<StatusKanbanOrder> list = statusKanbanOrderRepository.findByArtist(sk.getArtist().getId().intValue());
-            //Recorro los statuskanban order de cada artista para recolocarlos
             for (StatusKanbanOrder sk2 : list) {
                     if(sk.getOrder()>order){
-                        //Si el orden es mayor que el nuevo orden, tengo que bajar el resto sumándoles 1, hasta que lleguen a la posición del orden antiguo
                         if(sk2.getOrder()>=order && sk2.getOrder()<sk.getOrder()){
                             sk2.setOrder(sk2.getOrder()+1);
                             statusKanbanOrderRepository.save(sk2);
                         }
                     }else{
-                        //Si el orden es menor que el nuevo orden, tengo que subir el resto restándoles 1, hasta que lleguen a la posición del orden antiguo
-                        //No pueden ser iguales porque fuera he descartado ese caso
                         if(sk2.getOrder()<=order && sk2.getOrder()>sk.getOrder()){
                             sk2.setOrder(sk2.getOrder()-1);
                             statusKanbanOrderRepository.save(sk2);
                         }
                     }
-                    
                 }
                 sk.setOrder(order);
         }return statusKanbanOrderRepository.save(sk);
     }
 
     @Transactional(readOnly = true)
-	public StatusKanbanOrder findStatusKanbanOrder(Integer statusKanbanOrderId) {
-		return statusKanbanOrderRepository.findById(statusKanbanOrderId).get();
-	}
+    public StatusKanbanOrder findStatusKanbanOrder(Integer statusKanbanOrderId) {
+        return statusKanbanOrderRepository.findById(statusKanbanOrderId).get();
+    }
 
     @Transactional(readOnly = true)
-	public List<StatusKanbanOrder> findAllStatusKanbanOrder() {
-		return statusKanbanOrderRepository.findAll();
-	}
+    public List<StatusKanbanOrder> findAllStatusKanbanOrder() {
+        return statusKanbanOrderRepository.findAll();
+    }
 
     @Transactional(readOnly = true)
-	public List<StatusKanbanOrder> findAllStatusKanbanOrderByArtist(Integer artistId) {
-		return statusKanbanOrderRepository.findByArtist(artistId);
-	}
+    public List<StatusKanbanOrder> findAllStatusKanbanOrderByArtist(Integer artistId) {
+        return statusKanbanOrderRepository.findByArtist(artistId);
+    }
 
     @Transactional
-public void deleteStatusKanbanOrder(Integer id) {
-    StatusKanbanOrder statusToDelete = statusKanbanOrderRepository.findById(id).get();
+    public void deleteStatusKanbanOrder(Integer id) {
+        StatusKanbanOrder statusToDelete = statusKanbanOrderRepository.findById(id).get();
 
-    Integer artistId = statusToDelete.getArtist().getId().intValue();
-    Integer orderDeleted = statusToDelete.getOrder();
+        Integer artistId = statusToDelete.getArtist().getId().intValue();
+        Integer orderDeleted = statusToDelete.getOrder();
 
-    String color="";
-    String description="";
-    String nombre="";
-    statusToDelete.setColor(color);
-    statusToDelete.setDescription(description);
-    statusToDelete.setName(nombre);
+        String color="";
+        String description="";
+        String nombre="";
+        statusToDelete.setColor(color);
+        statusToDelete.setDescription(description);
+        statusToDelete.setName(nombre);
 
-    statusKanbanOrderRepository.deleteById(id);
+        statusKanbanOrderRepository.deleteById(id);
 
-    List<StatusKanbanOrder> statusList = statusKanbanOrderRepository.findByArtist(artistId);
+        List<StatusKanbanOrder> statusList = statusKanbanOrderRepository.findByArtist(artistId);
 
-    for (StatusKanbanOrder status : statusList) {
-        if (status.getOrder() > orderDeleted) {
-            status.setOrder(status.getOrder() - 1);
-            statusKanbanOrderRepository.save(status);
+        for (StatusKanbanOrder status : statusList) {
+            if (status.getOrder() > orderDeleted) {
+                status.setOrder(status.getOrder() - 1);
+                statusKanbanOrderRepository.save(status);
+            }
         }
     }
-}
 
     @Transactional
     public StatusKanbanOrder updateOrder(StatusKanbanOrder statusKanbanOrder) {
