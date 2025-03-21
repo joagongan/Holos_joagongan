59815a435d1c87933cc6e291c248e666fd56e0dc
javmunrom <javmunrom@alum.us.es>
2025-03-21 11:51:52 +0100

    Explorar completo para movil, faltan estilos ordenador

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,1 +1,16 @@
-export const API_URL = "https://holos-s2.onrender.com/api/v1";
+import { Platform } from "react-native";
+
+let BASE_URL: string = "";
+
+if (__DEV__) {
+  if (Platform.OS === "web") {
+    BASE_URL = "http://localhost:8080";
+  } else {
+    BASE_URL = "http://10.0.2.2:8080";
+  }
+} else {
+  BASE_URL = "https://holos-s2.onrender.com";
+}
+
+export const API_URL: string = `${BASE_URL}/api/v1`;
+export { BASE_URL };
