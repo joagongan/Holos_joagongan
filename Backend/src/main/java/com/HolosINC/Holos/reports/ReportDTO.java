4daad5a07ff40dcf76f5af4df8a20ee50349b217
juanant03 <juamormog@gmail.com>
2025-03-21 13:26:44 +0100

    fix: deleted reportstatus

--- Backend/src/main/java/com/HolosINC/Holos/reports/ReportDTO.java
+++ Backend/src/main/java/com/HolosINC/Holos/reports/ReportDTO.java
@@ -1,27 +1,15 @@
 package com.HolosINC.Holos.reports;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class ReportDTO {
     
-    @Size(max = 50)
-    @NotNull
     private String name;
 
-    @Size(max = 255)
-    @NotNull
     private String description;
-
-    @NotNull
-    private ReportStatus status;
     
-    @NotNull
     private Long workId;
 
-    @Size(max = 50)
-    @NotNull
     private String reportType;
 }
