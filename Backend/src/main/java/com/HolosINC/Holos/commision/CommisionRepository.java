713b286e63527aa2cb493c20ab9f32ff7eaa1090
jose-maria-portela <92798583+jose-maria-portela@users.noreply.github.com>
2025-03-22 01:35:03 +0100

    feat: Añadido la adición automática al Kanban de la comisión
    Corregido además la forma de obtener el id del Artista, pues antes era más fácil de hackear

--- Backend/src/main/java/com/HolosINC/Holos/commision/CommisionRepository.java
+++ Backend/src/main/java/com/HolosINC/Holos/commision/CommisionRepository.java
@@ -1,12 +1,19 @@
 package com.HolosINC.Holos.commision;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.HolosINC.Holos.Kanban.StatusKanbanOrder;
 
 @Repository
 public interface CommisionRepository extends JpaRepository<Commision, Long>{
     
     @Query("SELECT COUNT(c) FROM Commision c WHERE c.artist.id = :artistId AND c.status = 'ACCEPTED'")
     Integer numSlotsCovered(Long artistId);
+
+    @Query("SELECT s FROM StatusKanbanOrder s WHERE s.order = (SELECT MIN(s.order) FROM StatusKanbanOrder s)")
+    Optional<StatusKanbanOrder> getFirstStatusKanbanOfArtist(Long artistId);
 }
