19ca724e5e019d756bf9175e8439bbf4657347ad
jdeljunco23 <jdeljuncoobregon@gmail.com>
2025-03-22 13:16:11 +0100

    feat: make ImageHandler injectable as a Spring component

--- Backend/src/main/java/com/HolosINC/Holos/util/ImageHandler.java
+++ Backend/src/main/java/com/HolosINC/Holos/util/ImageHandler.java
@@ -2,10 +2,12 @@ package com.HolosINC.Holos.util;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.HolosINC.Holos.exceptions.ResourceNotFoundException;
 
+@Component
 public class ImageHandler {
     
     public byte[] getBytes(MultipartFile imageProfile) {
