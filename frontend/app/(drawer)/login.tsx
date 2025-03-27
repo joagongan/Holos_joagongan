9e5a2943ea7da219bb89423d722674cc5d598c60
mgomezvela <mgomezvela0@gmail.com>
2025-03-27 19:27:22 +0100

    chequeo

--- frontend/app/(drawer)/login.tsx
+++ frontend/app/(drawer)/login.tsx
@@ -120,6 +120,7 @@ export default function LoginScreen ({ navigation }:any) {
 
 };
 
+// cambio 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', paddingHorizontal: 20, alignSelf: 'center', maxWidth: 1600, minWidth: '60%' },
   title: { fontSize: 24, textAlign: 'center', marginBottom: 20 },
