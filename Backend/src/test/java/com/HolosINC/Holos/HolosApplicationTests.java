da160deeeb04dda63d1c0fcab281b7b67f9f12fd
Joaquín González Ganfornina <joagongan@alum.us.es>
2025-04-08 01:45:25 +0200

    Descomentar el contextLoads()

--- Backend/src/test/java/com/HolosINC/Holos/HolosApplicationTests.java
+++ Backend/src/test/java/com/HolosINC/Holos/HolosApplicationTests.java
@@ -3,12 +3,9 @@ package com.HolosINC.Holos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class HolosApplicationTests {
-
-/* 	@Test
+ 	@Test
 	void contextLoads() {
-	} */
-
-
+	} 
 }
