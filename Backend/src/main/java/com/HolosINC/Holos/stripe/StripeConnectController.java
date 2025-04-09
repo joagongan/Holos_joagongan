e8f30e01c002f259a5338f99402b330670851017
Joaquín González Ganfornina <joagongan@alum.us.es>
2025-04-10 00:28:24 +0200

    fix: Cambiar artistService por artistRepository en controlador y servicio de Stripe Connect

--- Backend/src/main/java/com/HolosINC/Holos/stripe/StripeConnectController.java
+++ Backend/src/main/java/com/HolosINC/Holos/stripe/StripeConnectController.java
@@ -1,12 +1,9 @@
 package com.HolosINC.Holos.stripe;
 
-import com.stripe.exception.StripeException;
-import com.stripe.model.Account;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,27 +21,6 @@ public class StripeConnectController {
     @Autowired
     public StripeConnectController(StripeConnectService stripeConnectService) {
         this.stripeConnectService = stripeConnectService;
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<Account>> getAllConnectedAccounts() {
-        try {
-            List<Account> accounts = stripeConnectService.getAllConnectedAccounts();
-            return new ResponseEntity<>(accounts, HttpStatus.OK);
-        } catch (StripeException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/{accountId}")
-    public ResponseEntity<String> getAccountById(@PathVariable String accountId){
-        try {
-            Account account= stripeConnectService.getById(accountId);
-            String accountStr = account.toJson();
-            return new ResponseEntity<String>(accountStr, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
     }
 
     @PostMapping("/create")
