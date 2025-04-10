92e7176991ab3f104339931e4614c2a928e4fe26
Joaquín González Ganfornina <joagongan@alum.us.es>
2025-04-10 08:37:08 +0200

    fix: Refactorizar un poco Client

--- Backend/src/main/java/com/HolosINC/Holos/client/ClientRepository.java
+++ Backend/src/main/java/com/HolosINC/Holos/client/ClientRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.HolosINC.Holos.artist.Artist;
-
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
@@ -15,11 +13,9 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     boolean isClient(Long id);
     
     @Query("SELECT c FROM Client c WHERE c.baseUser.id = :id")
-    Optional<Client> getClientByUser(Long id);
+    Optional<Client> findClientByUserId(Long id);
 
     @Query("SELECT COUNT(c) > 0 FROM Commision c WHERE c.client.id = :clientId AND c.status = 'ACCEPTED'")
     boolean hasActiveCommisions(Long clientId);
 
-    @Query("SELECT c FROM Client c WHERE c.baseUser.id = :id")
-   Optional<Artist> findClientByUser(Long id);
 }
